33b505d719b923ae6c251b334836d5a16b4a4241
Viannaiv <viva95@windowslive.com>
2023-06-26 18:59:18 +0300

    Exercise 13.16

---
+++ controllers/authors.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+const { sequelize } = require('../util/db')
+
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll({
+        attributes: [
+            'author',
+            [sequelize.fn('COUNT', '*'), 'blogs'],
+            [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+        ],
+        group: ['author'],
+        order: [
+            ['likes', 'DESC']
+        ]
+    })
+    res.json(blogs)
+})
+
+module.exports = router
